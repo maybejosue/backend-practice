a99789ccf4cf3b3fa9225213bb808309b56897b7
Josue Rodriguez <smexyjosue@gmail.com>
2020-03-19 15:43:46 -0700

    Add basic structure to db
    * Added api, characters, and data directories
    
    * Added index.js and server.js

--- api/server.js
+++ api/server.js
@@ -4,3 +4,10 @@ const helmet = require("helmet");
 const server = express();
 
 server.use(helmet());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("<p>Running Server</p>");
+});
+
+module.exports = server;
